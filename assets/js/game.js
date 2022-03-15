692d8f631ddc841c5905231dd2cb8e78b8a2d9e8
djteiji <djteiji@gmail.com>
2022-03-15 00:25:02 -0700

    add round numbers and game over notification

--- assets/js/game.js
+++ assets/js/game.js
@@ -74,7 +74,19 @@ var fight = function(enemyName) {
 
 
 for(var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
+    if (playerHealth > 0) {
+        window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+        var pickedEnemyName = enemyNames[i];
+    
+        enemyHealth = 50;
+        // use debugger to pause script from running and check what's going on at that moment in the code
+    // debugger;
+
+    // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
     fight(pickedEnemyName);
+    } else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+      }
+    
   }
